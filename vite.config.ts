80eb6abf35fb9569c4daca6052c933c7298a0f9e
HARINI_S_27 <harinisrinivasan2716@gmail.com>
2026-08-08 09:34:37 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/KATEB-AI/',
+
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
